372472c256b43c950f6a154c72d44ddd0da6b1c4
GugiChu <gauravchhajer613nkh@gmail.com>
2026-05-03 15:06:44 +0530

    MVP ready

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "react-markdown",
+    ],
+  },
+};
+
+export default nextConfig;
